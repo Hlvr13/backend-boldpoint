3bbd549db69bda9b0b376def746f45f690338963
LeviVazquez <hlvr.13@gmail.com>
2020-06-09 00:07:39 -0700

    :hammer:[Helpers/MongoDB] Added initial files

---
+++ mongodb/index.js
@@ -0,0 +1,2 @@
+// TODO: Usually, I use MLAB for Cloud Database storage. Pretty simple to use and gives a sandbox config. 
+// TODO: Will be ideal for the rate/favorite news. Let me know if you want an example
